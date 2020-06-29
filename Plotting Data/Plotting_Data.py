0408ca00a0412ee2c1af7a1128366299ee54b53f
Jhonattanln <63247251+Jhonattanln@users.noreply.github.com>
2020-06-29 19:21:43 -0300

    Update Plotting_Data.py

--- Plotting Data/Plotting_Data.py
+++ Plotting Data/Plotting_Data.py
@@ -2,3 +2,21 @@
 
 import pandas as pd
 import matplotlib.pyplot as plt
+
+#Dados
+prbr = pd.read_excel(r'C:\Users\Jhona\OneDrive\Área de Trabalho\PRBR11.xlsx', index_col='Data')
+
+#Histograma de distribuição
+
+
+cple = prbr['CPLE6']
+percent = cple.pct_change()
+plt.hist(percent, bins=25)
+plt.xlabel('Retornos')
+plt.ylabel('Frequência')
+plt.show()
+
+#Retornos
+plt.plot(percent, color='b')
+plt.show()
+
