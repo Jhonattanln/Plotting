d956ae2a9c5334c6d10ee36aabe5ca8e089a88fe
Jhonattanln <63247251+Jhonattanln@users.noreply.github.com>
2020-06-29 19:46:51 -0300

    Update Plotting_Data.py

--- Plotting Data/Plotting_Data.py
+++ Plotting Data/Plotting_Data.py
@@ -1,4 +1,4 @@
-## Importas as bibliotecas
+## Importar as bibliotecas
 
 import pandas as pd
 import matplotlib.pyplot as plt
@@ -9,9 +9,9 @@ prbr = pd.read_excel(r'C:\Users\Jhona\OneDrive\Área de Trabalho\PRBR11.xlsx', i
 #Histograma de distribuição
 
 
-cple = prbr['CPLE6']
+cple = prbr['CPLE6'].loc['2020-01-01':]
 percent = cple.pct_change()
-plt.hist(percent, bins=25)
+plt.hist(percent, bins=30, color='r')
 plt.xlabel('Retornos')
 plt.ylabel('Frequência')
 plt.show()
@@ -20,3 +20,15 @@ plt.show()
 plt.plot(percent, color='b')
 plt.show()
 
+#Scatter plot
+plt.scatter(prbr['RAIL3'], prbr['KLBN11'])
+plt.xlabel('RAIL3')
+plt.ylabel('KLBN11')
+plt.show()
+
+#Series
+fig, ax = plt.subplots()
+ax.plot(prbr.index, prbr['CPLE6'], prbr['KLBN11'])
+ax.set_xlabel('Data')
+ax.set_ylabel('Preço')
+plt.show()
