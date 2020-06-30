df5686f9d01d918cae47e39770ebb824cc53a5b3
Jhonattanln <63247251+Jhonattanln@users.noreply.github.com>
2020-06-29 21:11:35 -0300

    Update Plotting_Data.py

--- Plotting Data/Plotting_Data.py
+++ Plotting Data/Plotting_Data.py
@@ -11,22 +11,27 @@ prbr = pd.read_excel(r'C:\Users\Jhona\OneDrive\Área de Trabalho\PRBR11.xlsx', i
 
 cple = prbr['CPLE6'].loc['2020-01-01':]
 percent = cple.pct_change()
+plt.style.use('ggplot')
 plt.hist(percent, bins=30, color='r')
 plt.xlabel('Retornos')
 plt.ylabel('Frequência')
 plt.show()
 
 #Retornos
+plt.style.use('ggplot')
 plt.plot(percent, color='b')
 plt.show()
 
 #Scatter plot
-plt.scatter(prbr['RAIL3'], prbr['KLBN11'])
-plt.xlabel('RAIL3')
-plt.ylabel('KLBN11')
+plt.style.use('ggplot')
+fig, ax = plt.subplots()
+ax.scatter(prbr['CPLE6'], prbr['SAPR11'], c=prbr.index)
+ax.set_xlabel('CPLE6')
+ax.set_ylabel('SAPR11')
 plt.show()
 
 #Series
+plt.style.use('ggplot')
 fig, ax = plt.subplots()
 ax.plot(prbr.index, prbr['CPLE6'], prbr['KLBN11'])
 ax.set_xlabel('Data')
